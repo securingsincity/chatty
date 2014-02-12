dcd2666a74759f9f59525d3648db550a6145adb6
Robert Bergman <robert.bergman@gmail.com>
2014-02-12 15:52:33 -0700

    Add help script

---
+++ commands/chatty.js
@@ -0,0 +1,18 @@
+module.exports = function (commander) {
+
+  commander.on('chatty', {format: 'html'}, function (event, response) {
+    // TODO: generate this dynamically from docs on command modules
+    response.send(
+        '<b>Chatty commands</b>'
+      + '<pre>'
+      + '/chatty              this message<br>'
+      + '/img       [query]   find an image<br>'
+      + '/image     [query]   find an image<br>'
+      + '/anim      [query]   find an animated image<br>'
+      + '/animated  [query]   find an animated image<br>'
+      + '/face      [query]   find an image of a face<br>'
+      + '</pre>'
+    );
+  });
+
+};
