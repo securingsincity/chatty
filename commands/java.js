78f643d91d040cabf3048dd7dda93ce5995000df
Robert Bergman <robert.bergman@gmail.com>
2014-02-12 22:26:50 -0700

    Add response.random() fn

--- commands/java.js
+++ commands/java.js
@@ -1,10 +1,9 @@
-var request = require('request');
-var _ = require('lodash');
-
 module.exports = function (commander) {
 
   commander.on('java', function (event, response) {
-    response.send('https://s3.amazonaws.com/uploads.hipchat.com/10804/132391/qOJSVtvBWOLoorv/java.jpg');
+    response.random([
+      'https://s3.amazonaws.com/uploads.hipchat.com/10804/132391/qOJSVtvBWOLoorv/java.jpg'
+    ]);
   });
 
 };
