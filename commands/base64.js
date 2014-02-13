1138c3ede3e312f9b7a38db2c8cacd14b427a47d
Robert Bergman <robert.bergman@gmail.com>
2014-02-12 23:01:37 -0700

    More scripts and tweaks

---
+++ commands/base64.js
@@ -0,0 +1,16 @@
+module.exports = function (commander) {
+
+  commander.on(/b64|base64/, function (event, response) {
+    var match = /(encode|decode)\s+(.*)/.exec(event.input);
+    if (match) {
+      var value;
+      if (match[1] === 'encode') {
+        value = new Buffer(match[2]).toString('base64');
+      } else {
+        value = new Buffer(match[2], 'base64').toString('utf8');
+      }
+      response.send(value);
+    }
+  });
+
+};
--- commands/chatty.js
+++ commands/chatty.js
@@ -1,24 +1,28 @@
 module.exports = function (commander) {
 
   commander.on('chatty', {format: 'html'}, function (event, response) {
-    // TODO: generate this dynamically from docs on command modules
-    response.send(
-        '<b>Chatty commands</b>'
-      + '<pre>'
-      + '/chatty                this message<br>'
-      + '/img       [query]     find an image<br>'
-      + '/image     [query]     find an image<br>'
-      + '/anim      [query]     find an animated image<br>'
-      + '/animate   [query]     find an animated image<br>'
-      + '/face      [query]     find an image of a face<br>'
-      + '/meme      [topic]?    show a meme by topic or popularity<br>'
-      + '/java                  show us how you really feel<br>'
-      + '/shipit                show a squirrel pic<br>'
-      + '/ackbar                it\'s a trap!<br>'
-      + '/trap                  it\'s a trap!<br>'
-      + '/mustache  [url|query] mustachify a url or query result<br>'
-      + '</pre>'
-    );
+    // TODO: generate this dynamically from docs in command modules
+    var msg =
+      '<b>Commands</b>' +
+      '<pre>' +
+      [
+        '/chatty                              this message<br>',
+        '/img       [query]                   find an image<br>',
+        '/image     [query]                   find an image<br>',
+        '/anim      [query]                   find an animated image<br>',
+        '/animate   [query]                   find an animated image<br>',
+        '/face      [query]                   find an image of a face<br>',
+        '/meme      [topic]?                  show a meme by topic or popularity<br>',
+        '/java                                show us how you really feel<br>',
+        '/shipit                              show a squirrel pic<br>',
+        '/ackbar                              it\'s a trap!<br>',
+        '/trap                                it\'s a trap!<br>',
+        '/mustache  [url|query]               mustachify a url or query result<br>',
+        '/alot                                you like it alot?<br>',
+        '/base64    [encode|decode] [input]   base64 encode or decode input<br>',
+      ].sort().join('') +
+      '</pre>';
+    response.send(msg);
   });
 
 };
