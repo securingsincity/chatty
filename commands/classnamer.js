e65d0eeb1f85976a6510bf8e153f7625927324ca
Robert Bergman <robert.bergman@gmail.com>
2014-02-16 13:34:13 -0700

    Class namer script

---
+++ commands/classnamer.js
@@ -0,0 +1,21 @@
+var _ = require('lodash');
+var request = require('request');
+
+module.exports = function (commander, logger) {
+
+  commander.command({
+    name: 'classname',
+    help: 'Helpfully suggests class names',
+    action: function (event, response, store) {
+      request.get({
+        url: 'http://classnamer.com/index.txt'
+      }, function (err, res, body) {
+        if (err) return logger.error(err.stack || err);
+        if (body) {
+          response.send(body.trim());
+        }
+      });
+    }
+  });
+
+};
