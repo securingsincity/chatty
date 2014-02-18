05491a516c5b6751b0a89cde7b458b44e4fc825e
Robert Bergman <robert.bergman@gmail.com>
2014-02-18 13:54:07 -0700

    Add commands/spies help filters

--- commands/chatty.js
+++ commands/chatty.js
@@ -3,27 +3,30 @@ module.exports = function (commander, logger) {
   commander.command({
     name: 'chatty',
     help: 'Prints both commands and spies help',
+    args: '[<filter>]',
     opts: {format: 'html'},
     action: function (event, response) {
-      response.send(commander.help());
+      response.send(commander.help(null, event.input));
     }
   });
 
   commander.command({
     name: 'commands',
     help: 'Prints command help',
+    args: '[<filter>]',
     opts: {format: 'html'},
     action: function (event, response) {
-      response.send(commander.help('commands'));
+      response.send(commander.help('commands', event.input));
     }
   });
 
   commander.command({
     name: 'spies',
     help: 'Prints spies help',
+    args: '[<filter>]',
     opts: {format: 'html'},
     action: function (event, response) {
-      response.send(commander.help('spies'));
+      response.send(commander.help('spies', event.input));
     }
   });
 
