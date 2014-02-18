9abb4489a1c41a775c2a7916e02db50ffb7c08ca
Robert Bergman <robert.bergman@gmail.com>
2014-02-18 12:22:26 -0700

    Fixes for heroku

--- commands/chatty.js
+++ commands/chatty.js
@@ -2,11 +2,28 @@ module.exports = function (commander, logger) {
 
   commander.command({
     name: 'chatty',
-    args: '[commands|spies]',
-    help: 'Prints this help message',
+    help: 'Prints both commands and spies help',
     opts: {format: 'html'},
     action: function (event, response) {
-      response.send(commander.help(event.input));
+      response.send(commander.help());
+    }
+  });
+
+  commander.command({
+    name: 'commands',
+    help: 'Prints command help',
+    opts: {format: 'html'},
+    action: function (event, response) {
+      response.send(commander.help('commands'));
+    }
+  });
+
+  commander.command({
+    name: 'spies',
+    help: 'Prints spies help',
+    opts: {format: 'html'},
+    action: function (event, response) {
+      response.send(commander.help('spies'));
     }
   });
 
