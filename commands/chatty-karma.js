c3915a5558f8118fef8fdbc428cba3e253a5e82a
Robert Bergman <robert.bergman@gmail.com>
2014-02-22 15:34:26 -0700

    Chatty-karma

---
+++ commands/chatty-karma.js
@@ -0,0 +1,15 @@
+module.exports = function (commander, logger) {
+
+  commander.script({
+    help: 'A spy that helps Chatty fight back against negative karma'
+  });
+
+  commander.spy({
+    hear: /\bchatty\s?--(\s|$)/i,
+    help: 'Chatty is watching you',
+    action: function (event, response) {
+      response.send('I\'ll cut you!  http://i.imgur.com/C1PG0CM.png');
+    }
+  });
+
+};
