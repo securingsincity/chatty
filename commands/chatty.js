cee47350ad28c2808ef0b0e7f07c1484668f7dc2
Robert Bergman <robert.bergman@gmail.com>
2014-02-24 09:25:14 -0700

    Reduce chatty help commands to one

--- commands/chatty.js
+++ commands/chatty.js
@@ -9,53 +9,48 @@ module.exports = function (commander, logger) {
 
   commander.command({
     name: 'chatty',
-    help: 'High-level help for Chatty',
+    args: '[help|commands|spies]',
+    help: 'Chatty help',
     opts: {format: 'html'},
     action: function (event, response) {
-      var helpUrl = commander.addon.config.localBaseUrl() + '/help';
-      response.send(
-        '<pre>' +
-        'See <a href="' + helpUrl + '">all commands and spies</a>, or use:\n' +
-        cliff.stringifyRows([
-          [' ', '/commands', ' ', '[<filter>]', ' ', 'lists commands available in this room'],
-          [' ', '/spies',    ' ', '[<filter>]', ' ', 'lists spies active in this room']
-        ]) +
-        '</pre>'
-      );
-    }
-  });
-
-  commander.command({
-    name: 'commands',
-    args: '[<filter>]',
-    help: 'Prints command help',
-    opts: {format: 'html'},
-    action: function (event, response) {
-      commander.help({
-        tenant: event.tenant,
-        type: 'commands',
-        filter: event.input,
-        format: 'html'
-      }).then(function (help) {
-        response.send(help);
-      });
-    }
-  });
-
-  commander.command({
-    name: 'spies',
-    args: '[<filter>]',
-    help: 'Prints spies help',
-    opts: {format: 'html'},
-    action: function (event, response) {
-      commander.help({
-        tenant: event.tenant,
-        type: 'spies',
-        filter: event.input,
-        format: 'html'
-      }).then(function (help) {
-        response.send(help);
-      });
+      var matchl
+      if (!event.input || (match = /^help\b/i.exec(event.input))) {
+        var helpUrl = commander.addon.config.localBaseUrl() + '/help';
+        response.help('chatty', 'See <a href="' + helpUrl + '">all commands and spies</a>', [
+          'commands [<filter>] -- lists commands available in this room',
+          'spies    [<filter>] -- lists spies active in this room'
+        ]);
+        // response.send(
+        //   '<pre>' +
+        //   'See <a href="' + helpUrl + '">all commands and spies</a>\n' +
+        //   'Usage:<br>' +
+        //   cliff.stringifyRows([
+        //     [' ', '/chatty commands', ' ', '[<filter>]', ' ', 'lists commands available in this room'],
+        //     [' ', '/chatty spies',    ' ', '[<filter>]', ' ', 'lists spies active in this room']
+        //   ]) +
+        //   '</pre>'
+        // );
+      } else if (match = /^commands\s*(.*)/i.exec(event.input)) {
+        commander.help({
+          tenant: event.tenant,
+          type: 'commands',
+          filter: match[1],
+          format: 'html'
+        }).then(function (help) {
+          response.send(help);
+        });
+      } else if (match = /^spies\s*(.*)/i.exec(event.input)) {
+        commander.help({
+          tenant: event.tenant,
+          type: 'spies',
+          filter: match[1],
+          format: 'html'
+        }).then(function (help) {
+          response.send(help);
+        });
+      } else {
+        response.confused();
+      }
     }
   });
 
