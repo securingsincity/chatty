725ad087c7b98e963e7343af4e136e5b5c587c0f
Dave Elkan <delkan@atlassian.com>
2014-02-25 15:54:08 -0800

    Add some sanity

--- commands/reddit.js
+++ commands/reddit.js
@@ -1,30 +1,84 @@
 var _ = require('lodash');
-var redwrap = require('redwrap');
+var request = require('request');
 
 module.exports = function (commander, logger) {
+
+    var postTemplate = _.template("<b><%= title %></b> <%= url %>");
 
     commander.script({
         help: 'Call up all sorts of reddit schenanigans'
     });
 
     commander.spy({
-        hear: /^r\/([\w\-]+)/,
+        hear: /^r\/(top|hot|rising|controversial)?$/,
         help: 'Lists the top post from a subreddit',
-        action: getTopPostFromSubreddit
+        action: function(event, response) {
+            doRedditRequest({
+                sub: "all",
+                sort: event.captures.length ? event.captures[0] : ""
+            }, renderPosts(response, 1));
+        }
     });
 
-    var postFormat = _.template("<b><%= title %></b> <%= url %>")
+    commander.spy({
+        hear: /^r\/([\w\-]+)\/?(top|hot|rising|controversial)?$/,
+        help: 'Lists the top post from a subreddit',
+        action: function(event, response) {
+            var sub = (event.captures.length) ? event.captures[0] : "all";
+            var sort = (event.captures.length > 1) ? event.captures[1] : "";
+            doRedditRequest({
+                sub: sub,
+                sort: sort
+            }, renderPosts(response, 1));
+        }
+    });
 
-    function getTopPostFromSubreddit(event, response) {
-        var sub = event.captures[0];
-        redwrap.r(sub, function(err, data, resp) {
+    function renderPosts(response, count) {
+        return function(err, posts) {
             if (err) return logger.error(err.stack || err);
-            var posts = (data.data && data.data.children && data.data.children.length) ? data.data.children : [];
             if (posts.length) {
-                response.send(postFormat(posts[0].data));
+
+                var html = [];
+                for (var i = 0; i < count; i++) {
+                    if (i < posts.length) {
+                        html.push(postTemplate(posts[i].data))
+                    }
+                }
+
+                response.send(html.join("<hr>"));
             } else {
                 response.send("No posts in this subreddit. (sadpanda)");
             }
+        }
+    }
+
+    function doRedditRequest(params, callback) {
+
+        request.get({
+            url: getRedditUrlForParams(params),
+            json: true
+        }, function(err, res, json) {
+            if (err) {
+                return callback(err);
+            }
+
+            callback(null, (json.data && json.data.children && json.data.children.length) ? json.data.children : []);
         });
+    }
+
+    function getRedditUrlForParams(params) {
+        var url = "http://www.reddit.com/";
+
+        if (params.sub) {
+            url += "r/" + params.sub;
+        }
+
+        if (params.sort) {
+            url += "/" + params.sort;
+        }
+
+        url += "/.json";
+
+        return url;
     }
 };
