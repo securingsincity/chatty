43e0a7c58e2749017f70709567c7ea69c5e9bb7e
John Pfeiffer <jpfeiffer@atlassian.com>
2014-02-25 18:09:07 -0800

    continue playing with html

--- commands/reddit.js
+++ commands/reddit.js
@@ -61,7 +61,7 @@ module.exports = function (commander, logger) {
 
     function hipchatTest(match, event, response) {
  //       response.random([ 'https://s3.amazonaws.com/uploads.hipchat.com/10804/132391/qOJSVtvBWOLoorv/java.jpg' ]);
-        var msg = 'john test';
+        var msg = 'john <strong>test</strong>';
         response.send(msg);
     }
 
