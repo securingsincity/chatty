a8edc4736edf8532be6a9a844eeacf3bfa9f82a6
John Pfeiffer <jpfeiffer@atlassian.com>
2014-02-25 18:05:54 -0800

    continue playing with testing

--- commands/reddit.js
+++ commands/reddit.js
@@ -60,9 +60,9 @@ module.exports = function (commander, logger) {
     }
 
     function hipchatTest(match, event, response) {
-        response.random([
-            'https://s3.amazonaws.com/uploads.hipchat.com/10804/132391/qOJSVtvBWOLoorv/java.jpg'
-        ]);
+ //       response.random([ 'https://s3.amazonaws.com/uploads.hipchat.com/10804/132391/qOJSVtvBWOLoorv/java.jpg' ]);
+        var msg = 'john test';
+        response.send(msg);
     }
 
     function renderPosts(count, callback) {
