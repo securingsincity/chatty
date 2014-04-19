30684e68c02dd30882d05717ec1cd88bca1a94fd
James Hrisho <james.hrisho@gmail.com>
2014-04-19 19:12:08 -0400

    initial commit"

---
+++ commands/ban.js
@@ -0,0 +1,24 @@
+// Port of https://github.com/github/hubot-scripts/blob/master/src/scripts/alot.coffee
+
+module.exports = function (commander, logger) {
+
+  commander.script({
+    help: 'Fake Ban'
+  });
+
+  commander.spy({
+    name: 'ban',
+    args: '[<word>]',
+    help: 'Replies with a random ban message',
+    action: action
+  });
+
+  function action(event, response) {
+    response.random([
+      'dude i\'m so banning you right now',
+      event.input + ' you are so banned',
+      'guys i\'m banning '+ event.input
+    ]);
+  }
+
+};
