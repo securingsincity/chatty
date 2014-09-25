30f0fae49d1bdc81e3ad2ec5b9fdcc8c4c1c7919
James Hrisho <james.hrisho@gmail.com>
2014-09-25 14:22:41 -0400

    maxt-jira start

---
+++ commands/maxt-jira.js
@@ -0,0 +1,19 @@
+// Port of https://github.com/github/hubot-scripts/blob/master/src/scripts/alot.coffee
+
+module.exports = function (commander, logger) {
+
+  commander.script({
+    help: 'A spy for displaying the MAXT links'
+  });
+
+  commander.spy({
+    hear: /(MAXT-[0-9][0-9][0-9][0-9][0-9])(MAXT-[0-9][0-9][0-9][0-9])|(MAXT-[0-9][0-9][0-9])/i,
+    help: 'JIRA MAXT to key',
+    action: action
+  });
+
+  function action(event, response) {
+    console.log(event);
+    console.log(response);
+  }
+};
