62b3f96c2a2ee8c9c80ddd36e2c73b5754f1a83c
James Hrisho <james.hrisho@gmail.com>
2015-04-23 09:02:47 -0400

    Fern life

--- commands/fern.js
+++ commands/fern.js
@@ -16,8 +16,12 @@ module.exports = function (commander, logger) {
     response.random([
       'saucelabs? i fu**ing hate those guys - fern',
       'https://s3.amazonaws.com/uploads.hipchat.com/102551/759683/ehqDKCZFahvXAbP/ferntounge.gif',
+      'http://i.giphy.com/l0O9ymOliegzAg3iU.gif',
+      'https://lh3.googleusercontent.com/-xqhswG0BxwU/VTaTsB6zMlI/AAAAAAAANoQ/IN20q9pwnaQ/w1014-h1350-no/IMG_20150421_112354.jpg',
       'I\'m making a tindr for gifs - fern',
+      'It\'s Britney B!#&%! - fern',
       'lego.js - fern',
+      'F1 Racing? - fern',
       'styles and bugs - fern',
       'I wouldn\'t call it done per se - fern',
       'crosshit - fern',
